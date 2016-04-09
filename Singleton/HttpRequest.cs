d2cc9d246e8e99ebe844c4cc012b51b90e9630bd
csdz <csdzwang@gmail.com>
2016-04-09 13:42:33 +0800

    add base class which is Singleton of Component

---
+++ Singleton/HttpRequest.cs
@@ -0,0 +1,78 @@
+﻿using System; 
+using System.IO;
+using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic; 
+
+//新建一个HttpRequest单例
+class HttpRequest : Singleton<HttpRequest>
+{
+
+	// Get请求
+    public WWW GET(string url)
+    {
+        WWW www = new WWW (url);
+        StartCoroutine (WaitForRequest (www)); // 由于是继承自MonoBehaviour，所以可以调用协程函数StartCoroutine
+		while (!www.isDone) {}
+		return www;
+    }
+
+	// Post请求
+    public WWW POST(string url, Dictionary<string,string> post)
+    {
+        WWWForm form = new WWWForm();
+        foreach(KeyValuePair<String,String> post_arg in post)
+        {
+           form.AddField(post_arg.Key, post_arg.Value);//添加表单字段数据
+        }
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www));
+		while (!www.isDone) {}
+        return www; 
+    }
+
+	//枚举器，yield关键字作用
+    private IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        } else {
+            Debug.Log("WWW Error: "+ www.error);
+        }    
+    }
+
+	public Sprite LoadSpriteAssets(string fullPath)
+	{
+		string      finalPath;
+		WWW         localFile;
+		Texture2D     texture;
+		Sprite sprite;
+		
+		finalPath = "file://" + fullPath;
+		localFile = new WWW(finalPath);
+
+		StartCoroutine (WaitForRequest (localFile));
+
+		while (localFile.isDone) {}
+		
+		texture = localFile.texture;
+		return LoadSprite (texture);
+	}
+
+	public Sprite LoadSprite(string resPathWithoutExtension)
+	{
+		Texture2D tex = Resources.Load<Texture2D>(resPathWithoutExtension);
+		return LoadSprite (tex);
+	}
+
+	public Sprite LoadSprite(Texture2D tex)
+	{
+		Rect rec = new Rect(0, 0, tex.width, tex.height);
+		return Sprite.Create(tex, rec, new Vector2(0.5f, 0.5f));
+	}
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine; // Unity相关API
+using System.Collections;
+using System;
+
+// 泛型T，继承自MonoBehaviour, 保证了此单例是组件的基类
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	protected static T instance;
+	
+	static bool instantiated = false; 
+ 
+	/**
+	  Returns the instance of this singleton.
+	*/
+	public static T Instance //使用c#的get，set Property的写法
+	{
+		get
+      	{
+
+         	if(instance == null){ //第一次调用Singleton<T>.Instance时instance==null
+				if(instantiated)
+					return null;
+            	instance = (T) FindObjectOfType(typeof(T)); // 寻找object，找到后转为Component
+ 
+            	if (instance == null){ //如果没有找到
+					GameObject go = new GameObject();//新建GameObject
+					go.name = typeof(T).ToString();
+					instance = go.AddComponent<T>();//把T挂载到go上，
+					instantiated = true; //初始化成功
+				}
+			}
+
+			//Destroy instance if the singleton persists into the editor
+			if (Application.isEditor && !Application.isPlaying){ //持久化之前消除该GameObject
+				Destroy(instance);
+				instance = null;
+			}
+ 
+			return instance;
+		}
+	}	
+	
+	protected void RemoveInstance(){
+		Destroy(instance);
+		instance = null;
+	}
+	
+	public static bool exists{
+		set{}
+		get{return instance != null;}
+	}
+}
